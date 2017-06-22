c2cdb120c1d4cbee87fee4ffaf56eece83913bb5
Somnath Sikdar <somnath.sikdar@gmail.com>
2017-06-22 20:32:04 +0200

    tests ...

--- text_classification/classifier.py
+++ text_classification/classifier.py
@@ -1,23 +1,24 @@
 import numpy as np
+import os
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
 
 from text_classification.utils import load_training_data, error_rate, load_test_data, write_to_file, out_file
 
 
-def train_model(inputs, labels, chunk_size):
+def train_model(inputs, labels, chunk_size, hidden_layer_size):
     number_partitions = int(len(inputs) / chunk_size)
     folds = {(i * chunk_size, (i + 1) * chunk_size) for i in range(0, number_partitions)}
     err_rates = []
 
     classifier = None
-    #vectorizer = CountVectorizer(min_df=1)
-    vectorizer = TfidfVectorizer(min_df=1)
+    vectorizer = CountVectorizer(min_df=1)
+    # vectorizer = TfidfVectorizer(min_df=1)
     for val_set in folds:
         classifier = MLPClassifier(solver='lbfgs',
                                    activation='logistic',
                                    alpha=1e-5,
-                                   hidden_layer_sizes=(36, 2),
+                                   hidden_layer_sizes=hidden_layer_size,
                                    random_state=1)
 
 
@@ -50,10 +51,18 @@ if __name__ == '__main__':
     training_data, labels = load_training_data()
     test_data = load_test_data()
 
+    hidden_layer_size = [(i, j) for i in range(5, 30, 5) for j in range(1, 4)]
     chunk_size = len(test_data)
-    classifier, vectorizer, mean_err, sd_err = train_model(training_data, labels, chunk_size)
-    print("Mean error = ", mean_err)
-    print("SD error = ", sd_err)
+    err_file = os.path.join(dir, './data/error_rates.txt')
 
-    pred = predict(classifier, vectorizer, test_data)
-    write_to_file(out_file, pred)
+    with open(err_file, 'wb') as f:
+        for hidden_layer in hidden_layer_size:
+            classifier, vectorizer, mean_err, sd_err = train_model(training_data, labels, chunk_size)
+
+            message = "hidden layer size: {}".format(hidden_layer) + \
+                      " mean error = {}".format(mean_err) + " sd error = {}".format(sd_err)
+            f.write(message)
+            f.write("\n")
+
+            pred = predict(classifier, vectorizer, test_data)
+            write_to_file(out_file, pred)
