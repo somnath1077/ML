2e687007785b8f34c9959cbc6c95f2d2e9646302
Somnath Sikdar <somnath.sikdar@gmail.com>
2017-06-22 06:54:17 +0200

    error_rate function

--- text_classification/utils.py
+++ text_classification/utils.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+
 import os
+import numpy as np
 
 from typing import List
 from sklearn.feature_extraction.text import CountVectorizer
@@ -9,6 +12,13 @@ input_file = os.path.join(dir, './xtrain_obfuscated.txt')
 labels_file = os.path.join(dir, './ytrain.txt')
 trainings_file = os.path.join(dir, './xtest_obfuscated.txt')
 out_file = os.path.join(dir, './ytest.txt')
+
+def error_rate():
+    training_labels = fetch_data(labels_file)
+    predicted_labels = fetch_data(out_file)
+    err = [(i, j) for i, j in zip(training_labels, predicted_labels) if i != j]
+    err_rate = (len(err) / len(training_labels)) * 100
+    return err_rate
 
 def write_to_file(file, data):
     with open(file, 'wb') as f:
@@ -31,7 +41,7 @@ def classify(X, y):
     classifier = MLPClassifier(solver='lbfgs',
                                activation='logistic',
                                alpha=1e-5,
-                               hidden_layer_sizes = (5, 2),
+                               hidden_layer_sizes = (8, 2),
                                random_state = 1)
     classifier.fit(X, y)
     return classifier
@@ -56,3 +66,4 @@ if __name__ == '__main__':
     print("length of test_sentences = ", len(test_sentences))
     predictions = predict(classifier, X)
     write_to_file(out_file, predictions)
+    print("Error rate = ", error_rate())
