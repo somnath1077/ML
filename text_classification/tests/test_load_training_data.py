03775b432ce97585552c8c68745d2001208cd34f
Somnath Sikdar <somnath.sikdar@gmail.com>
2017-06-22 06:39:58 +0200

    first commit

---
+++ text_classification/tests/test_load_training_data.py
@@ -0,0 +1,5 @@
+from offline_challenge.utils import load_training_data
+
+
+def test_load_training_data():
+    load_training_data()
---
+++ text_classification/utils.py
@@ -0,0 +1,58 @@
+import os
+
+from typing import List
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neural_network import MLPClassifier
+
+dir = os.path.dirname(__file__)
+input_file = os.path.join(dir, './xtrain_obfuscated.txt')
+labels_file = os.path.join(dir, './ytrain.txt')
+trainings_file = os.path.join(dir, './xtest_obfuscated.txt')
+out_file = os.path.join(dir, './ytest.txt')
+
+def write_to_file(file, data):
+    with open(file, 'wb') as f:
+        for datum in data:
+            f.write(datum)
+
+def fetch_data(file):
+    lst = []
+    with open(file, 'rb') as f:
+        for line in f:
+            lst.append(line)
+    return lst
+
+def vectorize(sentences: List[str]):
+    vectorizer = CountVectorizer(min_df=1)
+    X = vectorizer.fit_transform(sentences)
+    return X
+
+def classify(X, y):
+    classifier = MLPClassifier(solver='lbfgs',
+                               activation='logistic',
+                               alpha=1e-5,
+                               hidden_layer_sizes = (5, 2),
+                               random_state = 1)
+    classifier.fit(X, y)
+    return classifier
+
+def predict(classifier, X):
+    return classifier.predict(X)
+
+def load_training_data():
+    sentences = fetch_data(input_file)
+    novels = fetch_data(labels_file)
+
+    assert len(sentences) == len(novels)
+    # print(len(sentences))
+    return sentences, novels
+
+if __name__ == '__main__':
+    sentences, novels = load_training_data()
+    X = vectorize(sentences)
+    classifier = classify(X, novels)
+    test_sentences = fetch_data(trainings_file)
+    X_1 = vectorize(test_sentences)
+    print("length of test_sentences = ", len(test_sentences))
+    predictions = predict(classifier, X)
+    write_to_file(out_file, predictions)
