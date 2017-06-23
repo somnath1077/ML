f5628cfa84736e45ec11324b2aa696d154923cd6
ssikdar <somnath.sikdar@zalando.de>
2017-06-23 10:47:41 +0200

    three layers...

--- text_classification/classifier.py
+++ text_classification/classifier.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     training_data, labels = load_training_data()
     test_data = load_test_data()
 
-    hidden_layer_size = [(i, i) for i in [10, 20, 30, 40]]
+    hidden_layer_size = [(i, i, i) for i in [10, 20, 30, 40]]
     chunk_size = len(test_data)
 
 
