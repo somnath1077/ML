c2cc383cbcd083a5b01f5f8f5cf11097f01781f7
ssikdar <somnath.sikdar@zalando.de>
2017-07-28 16:15:47 +0200

    started basic mnist tutorial

--- Tensorflow/mnist_basic.py
+++ Tensorflow/mnist_basic.py
@@ -1,3 +1,13 @@
+import tensorflow as tf
+
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
+
+# Placeholder variable to store an image (28 * 28 pixels = 784 pixels)
+x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
+
+# Variables for the weight matrix and bias
+W = tf.Variable(tf.zeros[784, 10])
+b = tf.Variable(tf.zeros[10])
+
