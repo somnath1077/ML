c1f3fa6d7d4ca6f3296a1685935f6b15472250c1
ssikdar <somnath.sikdar@zalando.de>
2017-07-28 17:38:30 +0200

    started basic mnist tutorial

--- Tensorflow/mnist_basic.py
+++ Tensorflow/mnist_basic.py
@@ -34,7 +34,6 @@ for _ in range(1000):
     session.run(train_step, feed_dict={x: batch_xs, y_actual: batch_ys})
 
 # Evaluation
-
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_actual, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print("Accuracy: ", session.run(accuracy, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels}))
