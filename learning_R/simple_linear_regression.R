445cf5167046c02ffd9dcb7fecadad56a78cb2b3
Somnath Sikdar <somnath.sikdar@gmail.com>
2018-01-10 09:07:57 +0100

    not much

--- learning_R/simple_linear_regression.R
+++ learning_R/simple_linear_regression.R
@@ -3,5 +3,10 @@ library(MASS)
 library(ISLR)
 
 lm.fit = lm(medv ~ lstat, data=Boston)
-print(summary(lm.fit))
 
+print(summary(lm.fit))
+print(names(lm.fit))
+print(coef(lm.fit))
+
+# Predict with 95% confidence interval
+predict(lm.fit, data.frame(lstat=c(5, 10, 15, 20)), interval="confidence")
